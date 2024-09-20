9bd75b28c0394edebe29dfef5322b59a0be9a512
BakedNut <joonkee1219@gmail.com>
2024-09-21 04:07:59 +0900

    New

---
+++ Cpp/27866.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    int a;
+
+    cin >> str;
+    cin >> a;
+
+    a--;
+
+    cout << str[a];
+
+}
