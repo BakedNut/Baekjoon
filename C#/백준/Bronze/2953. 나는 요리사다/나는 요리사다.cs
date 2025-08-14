152628041c783dc105ff9823c60a651e51226bd7
Junki Choi <joonkee1219@gmail.com>
2025-08-14 10:44:34 +0900

    [Bronze III] Title: 나는 요리사다, Time: 48 ms, Memory: 5524 KB -BaekjoonHub

---
+++ C#/백준/Bronze/2953. 나는 요리사다/나는 요리사다.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace boj_1000
+{
+    class Util
+    {
+        public static int findMax(Player[] players)
+        {
+            int maxScore = 0;
+            int whoisMax = 0;
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i].getScore() > maxScore)
+                {
+                    maxScore = players[i].getScore();
+                    whoisMax = i;
+                }
+            }
+
+            return whoisMax;
+        }
+    }
+    class Player
+    {
+        private int Score;
+
+        public Player(String s)
+        {
+            this.Score = 0;
+            int[] input = Array.ConvertAll(s.Split(" "), int.Parse);
+            
+            foreach (int k in input)
+            {
+                Score += k;
+            }
+        }
+
+        public int getScore()
+        {
+            return this.Score;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Player[] players = new Player[5];
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i] = new Player(Console.ReadLine());
+            }
+
+            int winner = Util.findMax(players);
+            
+            Console.WriteLine((winner + 1) + " " + players[winner].getScore());
+        }
+    }
+}
