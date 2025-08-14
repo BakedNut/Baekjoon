f84bcca681056655dff7c01ac760ca20ad3db384
Junki Choi <joonkee1219@gmail.com>
2025-08-14 10:50:20 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  List<String> list = new ArrayList<>();
+  String input;
+
+  public void IOprocess() throws IOException {
+    while((input = br.readLine()) != null) {
+      list.add(input);
+    }
+    for(String k : list) {
+      System.out.println(k);
+    }
+  }
+
+  public static void main(String[] Args) throws IOException {
+    Main app = new Main();
+    app.IOprocess();
+  }
+}
